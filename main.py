1e551c22f42582e186ec17e8019ec4ef2bd07fe0
jose eduardo lopes de souza <jose@qualitysistemas.com.br>
2019-11-08 13:12:14 -0400

    mostrando saldo

--- main.py
+++ main.py
@@ -5,23 +5,28 @@ print ('*********** Caixa Eletronico ***********')
 print ('****************************************')
 account_typed = input('Digite sua conta: ')
 password_typed = getpass.getpass('Digite sua senha: ')
-print (account_typed)
-print (password_typed)
 
 accounts_list = {
     '0001-02': {
         'password': '123456',
         'name': 'Fuladno da Silva',
-        'value': 0
+        'value': 1500
     },
     '0002-02': {
         'password': '123456',
         'name': 'Fulada de tal',
-        'value': 0
+        'value': 2000
     }
 }
 
 if  account_typed in accounts_list and password_typed == accounts_list[account_typed]['password']:
-    print('Conta valida')
+    print ('****************************************')
+    print ('*********** Caixa Eletronico ***********')
+    print ('****************************************')
+    print ('1 - saldo')
+    option_typed = input ('escolha uma das opcoes acima: ')
+    if option_typed == '1':
+        print('Seu saldo é %s' % accounts_list[account_typed]['value'])
+
 else:
     print ('Conta invalida')
