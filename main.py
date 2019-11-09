6c57aee5554dd65fd4618327760f4c6c9ef210fe
José Eduardo <jos3duadolopes@gmail.com>
2019-11-08 22:09:40 -0400

    deixar o programa sempre em execução

--- main.py
+++ main.py
@@ -1,32 +1,33 @@
 import getpass
 
-print ('****************************************')
-print ('*********** Caixa Eletronico ***********')
-print ('****************************************')
-account_typed = input('Digite sua conta: ')
-password_typed = getpass.getpass('Digite sua senha: ')
-
-accounts_list = {
-    '0001-02': {
-        'password': '123456',
-        'name': 'Fuladno da Silva',
-        'value': 1500
-    },
-    '0002-02': {
-        'password': '123456',
-        'name': 'Fulada de tal',
-        'value': 2000
-    }
-}
-
-if  account_typed in accounts_list and password_typed == accounts_list[account_typed]['password']:
+while True:
     print ('****************************************')
     print ('*********** Caixa Eletronico ***********')
     print ('****************************************')
-    print ('1 - saldo')
-    option_typed = input ('escolha uma das opcoes acima: ')
-    if option_typed == '1':
-        print('Seu saldo é %s' % accounts_list[account_typed]['value'])
+    account_typed = input('Digite sua conta: ')
+    password_typed = getpass.getpass('Digite sua senha: ')
 
-else:
-    print ('Conta invalida')
+    accounts_list = {
+        '0001-02': {
+            'password': '123456',
+            'name': 'Fuladno da Silva',
+            'value': 1500
+        },
+        '0002-02': {
+            'password': '123456',
+            'name': 'Fulada de tal',
+            'value': 2000
+        }
+    }
+
+    if  account_typed in accounts_list and password_typed == accounts_list[account_typed]['password']:
+        print ('****************************************')
+        print ('*********** Caixa Eletronico ***********')
+        print ('****************************************')
+        print ('1 - saldo')
+        option_typed = input ('escolha uma das opcoes acima: ')
+        if option_typed == '1':
+            print('Seu saldo é %s' % accounts_list[account_typed]['value'])
+
+    else:
+        print ('Conta invalida')
