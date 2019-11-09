3e9004c273037be71efb3869d219cb1e9890cbb9
José Eduardo <jos3duadolopes@gmail.com>
2019-11-09 10:40:59 -0400

    habilitando saque no caixa e finalizando

--- main.py
+++ main.py
@@ -43,6 +43,7 @@ while True:
         print ('*********** Caixa Eletronico ***********')
         print ('****************************************')
         print ('1 - Saldo')
+        print ('2 - Saque')
         if accounts_list[account_typed]['admin']:
             print ('10 - Incluir cédulas')
         option_typed = input ('escolha uma das opcoes acima: ')
@@ -50,9 +51,33 @@ while True:
             print('Seu saldo é %s' % accounts_list[account_typed]['value'])
         elif option_typed == '10' and accounts_list[account_typed]['admin']:
             amount_typed = input('Digite a quantidade de cédulas: ')
-            mpney_bill_typed = input('Digite a quantidade a ser incluido: ')
-            money_slips[amount_typed] += int(amount_typed)
-            print ('incluir cédulas')
+            money_bill_typed = input('Digite a quantidade a ser incluido: ')
+            # money_slips[mpney_bill_typed] = money_slips[amount_typed] + int(amount_typed)
+            money_slips[money_bill_typed] += int(amount_typed)
+            print (money_slips)
+        elif option_typed == '2':
+            value_typed = input('Digite o valor a ser sacado: ')
+
+            money_slips_user = {}
+            value_int = int(value_typed)
+
+            if value_int // 100 > 0 and value_int //100 <= money_slips['100']:
+                money_slips_user['100'] = value_int // 100
+                value_int = value_int - value_int // 100 * 100
+
+            if value_int // 50 > 0 and value_int //50 <= money_slips['50']:
+                money_slips_user['50'] = value_int // 50
+                value_int = value_int - value_int // 50 * 50
+
+            if value_int // 20 > 0 and value_int //20 <= money_slips['20']:
+                money_slips_user['20'] = value_int // 20
+                value_int = value_int - value_int // 20 * 20
+
+            if value_int != 0:
+                print ('O caixa não tem cédulas disponiveis para este valor')
+            else:
+                print ('Pegue as notas: ')
+                print (money_slips_user)
 
     else:
         print ('\n!!!!!!!!!!  Conta invalida  !!!!!!!!!!!!\n')
