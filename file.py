bd55b9223894cf20bd0ff83dd9beb0638225ac03
jose eduardo lopes de souza <jose@qualitysistemas.com.br>
2019-12-20 17:04:45 -0400

    lendo cedulas do caixa de um arquivo

--- file.py
+++ file.py
@@ -1,11 +1,48 @@
 import os
+from bank_account_variables import money_slips
+
 
 BASE_PATH = os.path.dirname(os.path.abspath(__file__))
-
 print(BASE_PATH)
 
-file = open(BASE_PATH + '/_file_teste.dat', 'w')
-file.write('Teste de escrita em arquivo')
-file.write('\n')
-file.write('Teste de escrita em arquivo')
-file.close()
+# file = open(BASE_PATH + '/_file_teste.dat', 'w')
+# file.write('Teste de escrita em arquivo')
+# file.write('\n')
+# file.write('Teste de escrita em arquivo')
+# file.close()
+
+
+def open_file_bank(mode):
+    return open(BASE_PATH + '/_bank_file.dat', mode)
+
+
+def write_money_slips(file):
+    for money_bill, value in money_slips.items():
+        file.write(money_bill + '=' + str(value) + ';')
+
+
+def read_money_slips(file):
+    line = file.readline()
+    while line.find(';') != -1:
+        semicolon_pos = line.find(';')
+        money_bill_value = line[0:semicolon_pos]
+        set_money_bill_value(money_bill_value)
+        # 20=5000;50=50000
+        if semicolon_pos + 1 == len(line):
+            break
+        else:
+            line = line[semicolon_pos+1:len(line)]
+
+
+def set_money_bill_value(money_bill_value):
+    equal_pos = money_bill_value.find('=') # 20=5000
+    money_bill = money_bill_value[0:equal_pos]
+    count_money_bill_value = len(money_bill_value)
+    value = money_bill_value[equal_pos + 1:count_money_bill_value]
+    print(money_bill, value)
+    money_slips[money_bill] = int(value)
+
+
+def load_bank_data():
+    file = open_file_bank('r')
+    read_money_slips(file)
--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import utils
 import operations
+from file import load_bank_data
+from bank_account_variables import money_slips
 
 
 def main():
+    load_bank_data()
+    print(money_slips)
     utils.header()
 
     account_auth = operations.auth_account()
 
-    if  account_auth:
+    if account_auth:
         utils.clear()
         utils.header()
 
         option_typed = operations.get_menu_options_typed(account_auth)
-        operations.do_operation(option_typed,account_auth)
+        operations.do_operation(option_typed, account_auth)
     else:
-        print ('\n!!!!!!!!!!  Conta invalida  !!!!!!!!!!!!\n')
+        print('\n!!!!!!!!!!  Conta invalida  !!!!!!!!!!!!\n')
+
 
 # enquanto o while for true o programa fica em execução
 while True:
